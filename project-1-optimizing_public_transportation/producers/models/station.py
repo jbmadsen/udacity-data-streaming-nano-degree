1e956f6131b3b8beac286dbbdfd58de16750c144
Jacob B. Madsen <jacob@jbmadsen.com>
2021-06-10 21:56:25 +0200

    Added the "FIXME"'s needed to complete this for future me

--- project-1-optimizing_public_transportation/producers/models/station.py
+++ project-1-optimizing_public_transportation/producers/models/station.py
@@ -1,4 +1,5 @@
 """Methods pertaining to loading and configuring CTA "L" station data."""
+
 import logging
 from pathlib import Path
 
@@ -16,9 +17,8 @@ class Station(Producer):
 
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_key.json")
 
-    #
     # TODO: Define this value schema in `schemas/station_value.json, then uncomment the below
-    #
+    # !FIXME
     #value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/arrival_value.json")
 
     def __init__(self, station_id, name, color, direction_a=None, direction_b=None):
@@ -31,13 +31,9 @@ class Station(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
-        topic_name = f"{station_name}" # TODO: Come up with a better topic name
+        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of replicas
+        # !FIXME
+        topic_name = f"{station_name}" # TODO: Come up with a better topic name (Why. It's a good name)
         super().__init__(
             topic_name,
             key_schema=Station.key_schema,
@@ -57,11 +53,8 @@ class Station(Producer):
 
     def run(self, train, direction, prev_station_id, prev_direction):
         """Simulates train arrivals at this station"""
-        #
-        #
         # TODO: Complete this function by producing an arrival message to Kafka
-        #
-        #
+        # !FIXME
         logger.info("arrival kafka integration incomplete - skipping")
         #self.producer.produce(
         #    topic=self.topic_name,
