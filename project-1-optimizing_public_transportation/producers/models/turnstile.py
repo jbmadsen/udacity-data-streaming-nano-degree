fa49f5e25c525aa405e65c7f1486436c1fcae45c
Jacob B. Madsen <jacob@jbmadsen.com>
2021-06-11 22:12:11 +0200

    Completed TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
    And added FIXME's for the remaning TODO's I need to remember tomorrow

--- project-1-optimizing_public_transportation/producers/models/turnstile.py
+++ project-1-optimizing_public_transportation/producers/models/turnstile.py
@@ -14,12 +14,8 @@ logger = logging.getLogger(__name__)
 class Turnstile(Producer):
     key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
 
-    #
     # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
-    #
-    #value_schema = avro.load(
-    #    f"{Path(__file__).parents[0]}/schemas/turnstile_value.json"
-    #)
+    value_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_value.json")
 
     def __init__(self, station):
         """Create the Turnstile"""
@@ -31,12 +27,8 @@ class Turnstile(Producer):
             .replace("'", "")
         )
 
-        #
-        #
-        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
-        # replicas
-        #
-        #
+        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of replicas
+        # !FIXME
         super().__init__(
             f"{station_name}", # TODO: Come up with a better topic name
             key_schema=Turnstile.key_schema,
@@ -50,10 +42,8 @@ class Turnstile(Producer):
     def run(self, timestamp, time_step):
         """Simulates riders entering through the turnstile."""
         num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
-        logger.info("turnstile kafka integration incomplete - skipping")
-        #
-        #
         # TODO: Complete this function by emitting a message to the turnstile topic for the number
         # of entries that were calculated
-        #
-        #
+        # !FIXME
+        logger.info("turnstile kafka integration incomplete - skipping")
+        
